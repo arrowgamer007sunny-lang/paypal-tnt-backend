ce78b9412b265b7f12270f99f7457d32c3da8816
arrowgamer007sunny-lang <arrowgamer007sunny@gmail.com>
2026-05-19 13:14:25 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+let donations = [];
+
+app.post("/paypal-webhook", (req,res)=>{
+
+   const event = req.body;
+
+   if(event.event_type === "PAYMENT.CAPTURE.COMPLETED"){
+
+      const amount =
+      parseFloat(event.resource.amount.value);
+
+      const username =
+      event.resource.custom_id || "Anonymous";
+
+      donations.push({
+         id: Date.now(),
+         username,
+         amount
+      });
+
+      console.log("New Donation:", username, amount);
+   }
+
+   res.sendStatus(200);
+});
+
+app.get("/paypal-donations.json",(req,res)=>{
+   res.json({donations});
+});
+
+app.listen(3000,()=>{
+   console.log("Server running");
+});
